64935c33346c2779d1f45b3279d1ff9d800bac26
amirebm <41531088+amirebm@users.noreply.github.com>
2024-08-23 13:01:29 +0200

    initial commit for front side including neccesary folders and css files

--- src/main/java/com/starthotel/hotelportal/repository/RoomRepository.java
+++ src/main/java/com/starthotel/hotelportal/repository/RoomRepository.java
@@ -1,4 +1,7 @@
 package com.starthotel.hotelportal.repository;
 
-public interface RoomRepository {
+import com.starthotel.hotelportal.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room,Long> {
 }
--- src/main/java/com/starthotel/hotelportal/service/RoomService.java
+++ src/main/java/com/starthotel/hotelportal/service/RoomService.java
@@ -1,6 +1,35 @@
 package com.starthotel.hotelportal.service;
 
-public class RoomService {
+import com.starthotel.hotelportal.model.Room;
+import com.starthotel.hotelportal.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.SQLException;
+@Service
+@RequiredArgsConstructor
+public class RoomService implements IRoomService {
+
+    private final RoomRepository roomRepository;
+@Override
+    public Room addNewRoom(MultipartFile file, String roomType, BigDecimal roomPrice) throws IOException, SQLException {
+    Room room= new Room();
+    room.setRoomType(roomType);
+    room.setRoomPrice(roomPrice);
+    if(!file.isEmpty()){
+        byte[] photoBytes= file.getBytes();
+        Blob photoBlob = new SerialBlob(photoBytes);
+        room.setPhoto(photoBlob);
+
+    }
+    return roomRepository.save(room);
+}
 
 
 }
